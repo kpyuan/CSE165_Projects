31e8dbe77f7db3ed714e35a29cae253106fa1c50
Sean G <Sean G>
2017-01-22 01:57:06 -0800

    added basic cannon functionality

---
+++ Assets/cannon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon : MonoBehaviour {
+    public Rigidbody cannonball;
+    Camera cam;
+	void Start () {
+        Debug.Log("loading cannon script");
+        cam = Camera.main;
+        StartCoroutine(Fire());
+    }
+	
+	void Update () {
+	}
+
+    IEnumerator Fire() {
+        yield return new WaitForSeconds(5);
+        Rigidbody cannonballClone = (Rigidbody) Instantiate(cannonball, new Vector3(0, 4, 0), Quaternion.identity);
+        cannonballClone.velocity = new Vector3(0, 5, 25);
+    }
+}
--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class spawner : MonoBehaviour {
     public GameObject brick;
 	void Start () {
-        Debug.Log("Hello World");
-        layLayer(24, 8.1f, 0.5f, 0.5f, 10);
+        Debug.Log("loading spawner script");
+        //StartCoroutine(layLayer(24, 8.1f, 0.5f, 0.5f, 10));
+        StartCoroutine(layLayer(48, 15.8f, 0.5f, 0.5f, 14));
     }
 	
 	void Update () {
 	}
 
-    void layCircle(int count, float radius, float height, float offset) {
+    IEnumerator layCircle(int count, float radius, float height, float offset) {
         for(int i = 0; i < count; i++) {
+            yield return new WaitForSeconds(0.05f);
             float angle = (i+offset) * Mathf.PI * 2 / count;
             Vector3 position = new Vector3(Mathf.Cos(angle)*radius, height, Mathf.Sin(angle)*radius);
             GameObject brickClone = (GameObject) Instantiate(brick, position, Quaternion.LookRotation(position-(new Vector3(0, height, 0))));
         }
     }
 
-    void layLayer(int count, float radius, float height, float offset, float layers) {
+    IEnumerator layLayer(int count, float radius, float height, float offset, float layers) {
         for (int i = 0; i < layers; i++) {
-            layCircle(count, radius, height+(1.0f*i), offset*i);
+            yield return new WaitForSeconds(0.1f);
+            StartCoroutine(layCircle(count, radius, height+(1.0f*i), offset*i));
         }
     }
 }

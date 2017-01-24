fab3436674d46ab45e96d8b3cffd417487d12d2c
Sean G <Sean G>
2017-01-23 22:42:33 -0800

    improved reticle, rewrote cannon raycasting for dynamic origin

--- Assets/cannon.cs
+++ Assets/cannon.cs
@@ -5,28 +5,24 @@ using UnityEngine;
 public class cannon : MonoBehaviour {
     public Rigidbody cannonball;
     Camera cam;
-    public static RaycastHit hit;
-    Ray ray;
-    public float offset;
 
     void Start() { 
         Debug.Log("loading cannon script");
         cam = Camera.main;
     }
+    
     void Update () {
-        ray = cam.ScreenPointToRay(new Vector3(Screen.width/2,Screen.height/2,0));
-
-        Physics.Raycast(ray, out hit);
-        //this will be according to camera gaze to
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(Fire());
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Fire();
         }
     }
 
-    IEnumerator Fire() {
-        yield return new WaitForSeconds(0.05f);
-        Rigidbody cannonballClone = (Rigidbody) Instantiate(cannonball, new Vector3(0, 4, 0), Quaternion.identity);
-        cannonballClone.velocity = new Vector3(hit.point.x ,hit.point.y+offset,hit.point.z);
+    void Fire() {
+        RaycastHit hit;
+        Ray sight = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        if (Physics.Raycast(sight, out hit, 100)) {
+            Rigidbody cannonballClone = (Rigidbody) Instantiate(cannonball, cam.transform.position, Quaternion.identity);
+            cannonballClone.velocity = sight.direction*25;
+        }
     }
 }

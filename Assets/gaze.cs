35cf0d95a6800f7792ac8fbe89d011329950ab3a
Sean G <Sean G>
2017-01-23 23:19:20 -0800

    replaced in-world reticle with UI reticle, refactored cannon.cs to gaze.cs

---
+++ Assets/gaze.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gaze : MonoBehaviour {
+    Camera cam;
+    public Rigidbody cannonball;
+
+    void Start() { 
+        Debug.Log("loading gaze script");
+        cam = Camera.main;
+    }
+    
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            FireCannon();
+        }
+    }
+
+    void FireCannon() {
+        //this code only fires when the player is looking directly at a physics object
+        /*RaycastHit hit;
+        Ray sight = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        if (Physics.Raycast(sight, out hit, 100) || true) {
+            Rigidbody cannonballClone = (Rigidbody) Instantiate(cannonball, cam.transform.position, Quaternion.identity);
+            cannonballClone.velocity = sight.direction*25;
+        }*/
+
+        //this code always fires
+        Rigidbody cannonballClone = (Rigidbody)Instantiate(cannonball, cam.transform.position, Quaternion.identity);
+        cannonballClone.velocity = cam.transform.forward * 25;
+    }
+}

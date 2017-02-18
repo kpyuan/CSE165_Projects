2b717d4e575cba5d0b976e58312d0ad325c96e09
Sean G <Sean G>
2017-02-18 02:51:02 -0800

    added basic checkpoint shape, added basic parsing code

---
+++ CSE165_Project3/Assets/loadCheckpoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class racetrack : MonoBehaviour {
+	void Start () {
+        generateTrack("track.txt");
+	}
+
+    void Update() {}
+
+    void generateTrack(string fileName) {
+        string[] lines = System.IO.File.ReadAllLines(fileName);
+        foreach (string line in lines){
+            string[] coordinates = line.Split();
+            Vector3 coordinate = new Vector3(float.Parse(coordinates[0]), float.Parse(coordinates[1]), float.Parse(coordinates[2]));
+            //create checkpoint gameobject
+            //somehow globally track all checkpoints
+        }
+    }
+}
---
+++ CSE165_Project3/Assets/racetrack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadCheckpoints : MonoBehaviour {
+	void Start () {
+        string[] lines = System.IO.File.ReadAllLines("track.txt");
+        foreach (string line in lines){
+            string[] coordinates = line.Split();
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
